9f4db642767ead788f2146675767c6c7761a8739
Vivek <vivekbaldaniya@gmail.com>
2021-07-09 23:25:46 +0530

    Change the firebase file

--- Firebase.js
+++ Firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase"
 
 var firebaseConfig = {
-    apiKey: "AIzaSyAQa4A_eeborGZhPcjRIDs8hw3y_DRzq0M",
-    authDomain: "daily-needs-143.firebaseapp.com",
-    projectId: "daily-needs-143",
-    storageBucket: "daily-needs-143.appspot.com",
-    messagingSenderId: "226494859020",
-    appId: "1:226494859020:web:5e66e8b87a0720c03aab29",
-    measurementId: "G-XNBB51QZED"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId
 };
 
 // Initialize Firebase
